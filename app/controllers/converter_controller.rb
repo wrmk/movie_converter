e664a43c498053805208eefbc02065ad40094af4
wrmk <elmakan@gmail.com>
2021-10-03 17:05:19 +0300

    some refactoring

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,14 +1,17 @@
 class ConverterController < ApplicationController
-  include Converter
 
   def upload
     @gif_name = SecureRandom.alphanumeric(12)
     gif_path = "app/assets/images/#{@gif_name}.gif"
-    convert(params[:movie].path, gif_path)
+    ConvertVideoJob.perform_later(params[:movie].path, gif_path)
+    # show_gif
+  end
+
+  def show_gif
     respond_to do |format|
       format.html {render "index"}
       format.js
-    end    
+    end     
   end
 
   private
