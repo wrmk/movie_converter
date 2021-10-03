a0ae9c3790ec2b38cbf1968f243a8bbd01e56110
wrmk <elmakan@gmail.com>
2021-10-03 19:27:30 +0300

    refactor convert

--- app/services/converter.rb
+++ app/services/converter.rb
@@ -1,9 +1,13 @@
 class Converter
   require 'streamio-ffmpeg'
 
-  def convert(movie_path,gif_path)
-    video = FFMPEG::Movie.new(movie_path)
-    options = {frame_rate: 5}
-    video.transcode(gif_path, options)
+  def convert(media)
+    gif_path = "tmp/file.gif"
+    media.open do |file|
+      video = FFMPEG::Movie.new(file.path)
+      options = {frame_rate: 5}
+      video.transcode(gif_path, options)
+      media.attach(io: File.open(gif_path),filename: "file.gif")
+    end
   end
 end
