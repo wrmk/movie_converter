0cdd213115fccc4b53bc72e43dfadb19ee42006b
wrmk <elmakan@gmail.com>
2021-10-06 19:15:08 +0300

    ignore active job

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -3,9 +3,12 @@ class ConverterController < ApplicationController
   def upload
     @storage = Storage.create storage_params
 
-    ConvertVideoJob.perform_later(@storage.id)
-    sleep 10
+    # ConvertVideoJob.perform_later(@storage.id) #ignore active job
+    # sleep 10
+    
+    Converter.new.convert(@storage.id)
     @storage.reload
+
     respond_to do |format|
       format.html {render "index"}
       format.js
